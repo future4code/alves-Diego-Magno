036c064e902998df29cba73da322aab1405fb63b
Diego Magno <dbmagno@yahoo.com.br>
2022-05-20 23:32:04 -0300

    inicio da revisao

---
+++ modulo1/revisao-postinho/index.js
@@ -0,0 +1,155 @@
+
+// COMPARADORES
+// Exercício 1------------------------------------------------------------------------------------
+
+// a-)Comparador de desigualdade a!==b 
+// Exemplo:
+
+// function checarDesigualdade(a, b) {
+
+//     return `No comparador de desigualdade ${a}!==${b} é ${a !== b}`
+// }
+// console.log(checarDesigualdade(1, 3));
+
+// b-)Compare a igualdade entre a===b
+
+//  Sua lógica aqui
+function checarIgualdade(a, b) {
+    
+    return `No comparador de igualdade ${a}=${b} é ${a === b}`
+}
+ console.log(checarIgualdade(3,3)); 
+
+// c-)Faça uma função chamada "verificaSeEMaior"
+
+console.log(verificaSeEMaior(321, 2156));
+
+
+// Exercício 2------------------------------------------------------------------------------------
+//Escreva true or false para as comparações abaixo:
+// exemplo 1>2 = false
+// a-) 1==='1'=
+// b-) 1=='1'=
+// c-) 'a'==='b'=
+// d-) 'b'>'a'=
+// e-) 0!==null=
+
+
+// CONDICIONAIS
+
+// Exercício 3------------------------------------------------------------------------------------
+
+const cadastro = () => {
+    const usuario = []
+
+    //  Sua lógica aqui
+
+}
+console.log(cadastro());
+
+// Exercício 4-----------------------------------------------------------------------------------------------
+
+const login = () => {
+    const login = "labenu"
+    //  Sua lógica aqui
+
+}
+
+console.log(login());
+
+// Exercício 5----------------------------------------------------------------------------------------------------
+
+const primeiraDose = () => {
+
+    //  Sua lógica aqui
+
+
+}
+console.log(primeiraDose())
+
+
+// LOOP+CONDICIONAL
+
+// Exercício 6 -------------------------------------------------------------------------------------
+
+const segundaDose = (nomeDoUsuario) => {
+    const usuarios = [
+        { nome: "Artur", imunizacao: "incompleta" },
+        { nome: "Barbara", imunizacao: "incompleta" },
+        { nome: "Carlos", imunizacao: "incompleta" }
+    ]
+
+    //  Sua lógica aqui
+
+
+}
+console.log(segundaDose("Barbara"));
+
+// Exercício 7 --------------------------------------------------------------------------------------
+
+const avisoAosAtrasados = () => {
+    const usuarios = [
+        { nome: "Artur", imunizacao: "incompleta" },
+        { nome: "Barbara", imunizacao: "completa" },
+        { nome: "Carlos", imunizacao: "incompleta" }
+    ]
+
+    //  Sua lógica aqui
+
+}
+console.log(avisoAosAtrasados());
+
+
+// DESAFIO------------------------------------------------------------------------------------------
+
+const usuarios = [
+    {
+        nome: "Artur",
+        ano: 2000,
+        nacionalidae: "brasileiro",
+        senha: "123456",
+        vacina: "pfizer",
+        imunizacao: "incompleta"
+    },
+    {
+        nome: "Bárbara",
+        ano: 1984,
+        nacionalidae: "brasileira",
+        senha: "labenu",
+        vacina: "astrazenica",
+        imunizacao: "completa"
+    },
+    {
+        nome: "Carlos",
+        ano: 2000,
+        nacionalidae: "brasileiro",
+        senha: "123456",
+        vacina: "coronavac",
+        imunizacao: "incompleta"
+    }
+
+]
+
+const cadastroDesafio = () => {
+    //  Sua lógica aqui
+}
+console.log(cadastroDesafio());
+
+const loginDesafio = () => {
+    //  Sua lógica aqui
+}
+console.log(loginDesafio());
+
+const primeiraDoseDesafio = () => {
+//  Sua lógica aqui
+}
+console.log(primeiraDoseDesafio())
+const segundaDoseDesafio = (nomeDoUsuario) => {
+    //  Sua lógica aqui
+}
+console.log(segundaDoseDesafio("ALGUM NOME AQUI"));
+
+const avisoAosAtrasadosDesafio = () => {
+    //  Sua lógica aqui
+}
+console.log(avisoAosAtrasadosDesafio());
